4690f71faab39f117fe8d9d20a48e79583ccf3f9
Youssef <rafrafi.youssef@hotmail.com>
2022-02-23 16:58:57 +0100

    binary

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from function import get_name, get_age, volume, odd_even, intersect, palyndrom, doublons, pgcd_ppcm, valid_ip, fibonacci
-# get_name()
-# get_age()
-# volume(1,1)
-# odd_even()
-
-# print(intersect([1, 2 , 5 , 7], [5 , 9 , 10 , 2]))
-# palyndrom("racecar")
-# doublons([1,2,5,6,7,7,7,7,5,55,6])
-# pgcd_ppcm(1225,800)
-# valid_ip(255)
-
-fibonacci()
+from function import binary
+binary()
